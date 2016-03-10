bb446840db27b0bfca43ddb2e470bc8dd955b96a
k-monkey <captain.monkey88@gmail.com>
2016-03-11 01:32:15 +0200

    added index.html, and inserted a custom search bar to it

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
+//setup static file in 'public' dir
+//look for file at http://localhost:5000/index.html
+//instead of http://localhost:5000/public/index.html
 app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
