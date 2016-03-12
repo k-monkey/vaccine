8b029a0d96a9ccb70f548b94131395665088c99e
DR.WHO <captain.monkey88@gmail.com>
2016-03-12 12:23:37 -0800

    linked age selector to table view using backbone.js

--- public/js/main-view.js
+++ public/js/main-view.js
@@ -2,4 +2,17 @@ var options = {
   valueNames: [ 'name', 'born' ]
 };
 
-var userList = new List('users', options);
+var userList = new List('vaccine-view', options);
+
+var VaccineView = Backbone.View.extend({
+    el: 'html', //root of the DOM //TODO: limit the view to vaccine-view div only
+    events: {
+        "change #age-selector": "filterByAge" 
+    },
+
+    filterByAge:function(e){
+        userList.search(this.$('#age-selector').val(), ['born']);
+    }
+});
+
+new VaccineView();
