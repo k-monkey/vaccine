2dca432f3983e14e5334d324de38bbc5302e151b
DR.WHO <captain.monkey88@gmail.com>
2016-03-13 15:43:22 -0700

    generate table from dummy data now

--- public/js/main-view.js
+++ public/js/main-view.js
@@ -1,8 +1,16 @@
-var options = {
-  valueNames: [ 'name', 'born' ]
+var dummyData = [{'vaccineId': '1', 'age_from': 0, 'country_code': 'USA', 'name': 'Hepatitis B', 
+'description': 'Hepatitis B virus (chronic inflammation of the liver, life-long complications)', 
+'dose': [1,3]},
+{'vaccineId': '2', 'age_from': 1, 'age_to': 2, 'country_code': 'USA', 'name': 'Hepatitis B', 
+'description': 'Hepatitis B virus (chronic inflammation of the liver, life-long complications)', 
+'dose': [2,3]}
+];
+
+var headerOptions = {
+  valueNames: [ 'age', 'name', 'dose', 'description']
 };
 
-var userList = new List('vaccine-view', options);
+var userList = new List('vaccine-view', headerOptions);
 
 var VaccineView = Backbone.View.extend({
     el: 'html', //root of the DOM //TODO: limit the view to vaccine-view div only
@@ -16,3 +24,23 @@ var VaccineView = Backbone.View.extend({
 });
 
 new VaccineView();
+
+function toTableRows(rawData) {
+	var rows = []
+	for (var idx in rawData) {
+		if (rawData[idx] != null) {
+			rows.push({
+				'age': rawData[idx]['age_from'],
+				'name': rawData[idx]['name'],
+				'dose': rawData[idx]['dose'][0] + ' of ' + rawData[idx]['dose'][1],
+				'description': rawData[idx]['description'],
+			});
+		}
+	}
+	return rows;
+}
+
+var vaccineViewRows = toTableRows(dummyData);
+for (var idx in vaccineViewRows) {
+	userList.add(vaccineViewRows[idx]);
+}
