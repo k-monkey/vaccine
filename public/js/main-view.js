8465958b6bcdaffc296287cfe8332759f138378c
DR.WHO <captain.monkey88@gmail.com>
2016-03-25 08:51:16 -0700

    implemented row filtering by age, using table.clear() and table.row.add()

--- public/js/main-view.js
+++ public/js/main-view.js
@@ -60,7 +60,9 @@ var genders = new ItemCollection([
     {name: "Male"}
 ]);
 var genderSelector = new ItemListView({el: $("#gender-selector"), 
-	collection: genders});
+	collection: genders,
+	clickAction: filterByGender
+});
 genderSelector.render();
 
 var ages = new ItemCollection([
@@ -72,7 +74,9 @@ var ages = new ItemCollection([
     {name: "5"}   
 ]);
 var ageSelector = new ItemListView({el: $("#age-selector"), 
-	collection: ages});
+	collection: ages,
+	clickAction: filterByAge
+});
 ageSelector.render();
 
 var countries = new ItemCollection([
@@ -82,17 +86,28 @@ var countries = new ItemCollection([
 ]);
 var countrySelector = new ItemListView({el: $("#add-country-selector"), 
 	collection: countries, 
-	clickAction: addNewTab,
+	clickAction: addNewTab
 });
 countrySelector.render();
 
+function filterByGender(gender) {
+	alert("filter by" + gender);
+	vaccineView.filterByGender(gender);
+}
 
-function toTableRows(rawData, countryCode) {
+function filterByAge(age) {
+	vaccineView.filterByAge(age);
+}
+
+function toTableRows(rawData, countryCode, filters) {
 	var rows = []
 	if (countryCode in rawData) {
 		var data = rawData[countryCode];
 		for (var idx in data) {
-			if (data[idx] != null) {
+			if (data[idx] == null || isFiltered(filters, data[idx])) {
+				continue;
+			}
+			else {
 				rows.push([
 					data[idx]['age_from'],data[idx]['name'],
 					data[idx]['dose'][0] + ' of ' + data[idx]['dose'][1]
@@ -101,6 +116,30 @@ function toTableRows(rawData, countryCode) {
 		}
 	}
 	return rows;
+}
+
+function isFiltered(filters, data) {
+	console.log("point 1");
+	if (filters == null) {
+		console.log("point 2");
+		return false;
+	}
+	else {
+		for (var fieldName in filters) {
+			console.log("point 3 " + fieldName);
+			switch (fieldName.toLowerCase()) {
+				case 'age': 
+					console.log("point 4 " + filters[fieldName]);
+					if (data['age_from'] > filters[fieldName]) {
+						return true;
+					}
+					break;
+				default:
+					continue;
+			}
+		}
+		return false;
+	}
 }
 
 
@@ -120,58 +159,74 @@ function toTableHeaders(data, countryCode) {
 	return headers;
 }
 
-//list of countries for which we display the vaccine view
-//we allow at most 3 countries
-var viewList = {'default-view': 'USA', '1st-view': null, '2nd-view': null};
-
-var VaccinePanel = Backbone.View.extend({
-    el: 'html', //root of the DOM //TODO: limit the view to vaccine-view div only
-    events: {
-        "change #age-selector": "filterByAge",
-    	"change #add-country-selector": "addTabFromSelector" 
-    },
-    filterByAge:function(e){
-        vaccineList.search(this.$('#age-selector').val(), ['age']);
-    },
-    addTabFromSelector: function() {
-    	addNewTab($('#add-country-selector').val());
-    }
-});
-
-new VaccinePanel();
-
 var VaccineTab = Backbone.View.extend({
-    initialize: function(){
+	options: {
+		filters: { //e.g.
+			//age: 1
+		}
+	},
+    initialize: function(options){
+    	if (options) {
+    		this.options = options;
+    	}
+    	else {
+    		this.options = {};
+    	}
     },
     render: function(tableId, countryCode) {
-		vaccineData = toTableRows(rawData, countryCode);
+		vaccineData = toTableRows(rawData, 
+				this.options.countryCode, 
+				this.options.filters);
+		var tableId = this.options.tableId;
 		$(document).ready(function() {
-		    $('#' + tableId).DataTable( {
-		        data: vaccineData,
+		    var table = $('#' + tableId).DataTable( {
+		        //data: vaccineData,
 		        searching: false,
 		        paging: false,
 		    	ordering:  false
 		    });
+		    table.clear();
+		    table.rows.add(vaccineData).draw();
 		} );    	
+    },
+    redraw: function(tableId, countryCode) {
+    	console.log("tab rendering " + Object.keys(this.options) );
+		vaccineData = toTableRows(rawData, 
+				this.options.countryCode, 
+				this.options.filters);
+		console.log("tab rendering2 " + vaccineData );
+		var tableId = this.options.tableId;
+		var table = $('#' + tableId).DataTable();    
+		table.clear();
+		table.rows.add(vaccineData).draw();	
     },
     events: {
         "click .add-tab": "addTab",
         "click .del-tab": "deleteTab",
     },
-    addTab: function() {
-    	addNewTab("CHN");
-    },
     deleteTab: function() {
     	this.remove();
+    },
+    filterByAge: function(age) {
+    	if (this.options.filters) {
+    		this.options.filters['age'] = age;
+    	}
+    	else {
+    		this.options['filters'] = {'age': age};
+    	}
+    	return this;
     }
 });
 
 //initiate the vaccine-view
 var VaccineView = Backbone.View.extend({
     el: $('#vaccine-view'),
-    variables: {},
+    options: {
+    	tabIdSeq: 0, 
+    	tabList: []
+    },
     initialize: function(){
-    	this.variables = {tabCount: 0, tabList: []}; 
+    	this.options = {tabIdSeq: 0, tabList: []}; 
     	this.render();
     },
     render: function() {
@@ -179,22 +234,38 @@ var VaccineView = Backbone.View.extend({
     },
     addTab: function(countryCode) {
     	//add the vaccine tab for default country "USA"
-    	var cId = this.variables.tabCount++;
+    	var cId = this.options.tabIdSeq++;
     	var containerId = "vaccine-tab-" + cId;
     	var tableId = "vaccine-table-" + cId;
     	var variables = {
     		tab_container_id: containerId, 
     		vaccine_table_id: tableId,
-    		country_code: countryCode};
+    		country_code: countryCode
+    	};
       	var template = _.template( $("#vaccine-tab-template").html());
       	this.$el.append( template(variables) );
       	if (cId == 0) {
       		this.$('.del-tab').remove();
       	}
 
-      	var newTab = new VaccineTab({el: $("#" + containerId)});
-      	newTab.render(tableId, countryCode);
-      	this.variables.tabList.push(newTab);
+      	var newTab = new VaccineTab({
+      		'el': $("#" + containerId),
+      		'tableId': tableId,
+      		'countryCode': countryCode
+      	});
+      	newTab.render();
+      	this.options.tabList.push(newTab);
+    },
+    filterByAge: function(age) {
+    	for (tabIdx in this.options.tabList) {
+    		var tab = this.options.tabList[tabIdx];
+    		if (tab != null) {
+    			tab.filterByAge(age).redraw();
+    		}
+    	}
+    },
+    filterByGender: function(gender) {
+    	alert("Filter by gender is not ready yet!");
     }
 });
 
